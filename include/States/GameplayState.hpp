d4dbacbf133706470b09131f05098b67812b551f
Reyhane Sanaei nezhad <reyhanesanaei25@gmail.com>
2026-07-22 19:49:54 +0330

    Update GameplayState.hpp

--- include/States/GameplayState.hpp
+++ include/States/GameplayState.hpp
@@ -2,11 +2,16 @@
 
 #include "Engine/StateManager.hpp"
 #include "Entities/Player.hpp"
+#include "Managers/MonsterManager.hpp"
+#include "Entities/Monster.hpp"
+#include "Bullets/Bullet.hpp"
 #include "Managers/CollisionManager.hpp"
 #include "Managers/PlatformManager.hpp"
 #include "UI/HUD.hpp"
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
 
 class Game;
 
@@ -22,8 +27,23 @@ public:
 private:
     Game& m_game;
     sf::Sprite m_background;
+
     Player m_player;
     PlatformManager m_platforms;
     CollisionManager m_collisions;
     HUD m_hud;
+    //phase2
+    std::vector<Bullet> m_bullets;
+    MonsterManager m_monsters; 
+    void spawnMonster(sf::Vector2f position, MonsterType type);
+    float m_shootCooldown = 0.f;
+    float m_monsterSpawnY = 350.f; 
+
+    sf::SoundBuffer m_shootBuffer;
+    sf::Sound m_shootSound;
+
+    sf::SoundBuffer m_jumpBuffer;
+    sf::Sound m_jumpSound;
+
 };
+;
